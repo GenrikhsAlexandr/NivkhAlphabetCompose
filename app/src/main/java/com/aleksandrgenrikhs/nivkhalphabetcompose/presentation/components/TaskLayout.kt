b5e42d5889005e6e3a81e1de89cb14dc2053598e
aleksandrgenrikhs <aleksandr.genrikhs@gmail.com>
2024-07-24 19:10:38 +0300

    fix TasksScreen

--- app/src/main/java/com/aleksandrgenrikhs/nivkhalphabetcompose/presentation/components/TaskLayout.kt
+++ app/src/main/java/com/aleksandrgenrikhs/nivkhalphabetcompose/presentation/components/TaskLayout.kt
@@ -16,7 +16,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Card
@@ -32,17 +32,21 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.aleksandrgenrikhs.nivkhalphabetcompose.R
-import com.aleksandrgenrikhs.nivkhalphabetcompose.model.TaskModel
 import com.aleksandrgenrikhs.nivkhalphabetcompose.presentation.ui.theme.NivkhAlphabetComposeTheme
 import com.aleksandrgenrikhs.nivkhalphabetcompose.presentation.ui.theme.colorCardLetterItem
 import com.aleksandrgenrikhs.nivkhalphabetcompose.presentation.ui.theme.colorOnPrimary
 import com.aleksandrgenrikhs.nivkhalphabetcompose.presentation.ui.theme.colorPrimary
+import com.aleksandrgenrikhs.nivkhalphabetcompose.presentation.ui.theme.colorProgressBar
 import com.aleksandrgenrikhs.nivkhalphabetcompose.presentation.ui.theme.colorText
 import com.aleksandrgenrikhs.nivkhalphabetcompose.presentation.ui.theme.shapes
 
 @Composable
 fun TaskLayout(
-    task: List<TaskModel>,
+    @StringRes titleResId: List<Int>,
+    @DrawableRes iconId: List<Int>,
+    isTaskCompleted: List<Boolean>,
+    isNextTaskVisible: List<Boolean>,
+    route: List<String>,
     letter: String,
     modifier: Modifier = Modifier,
     onClick: (String, String) -> Unit,
@@ -65,28 +69,32 @@ fun TaskLayout(
                 alignment = Alignment.Center
             )
         }
-        items(task) {
-            TaskItem(
-                task = it.task.titleResId,
-                iconResId = it.task.icon,
-                onTaskClick = {
-                    if (it.isNextTaskVisible) {
-                        onClick(it.task.route, letter)
-                    }
-                },
-                isClickable = it.isNextTaskVisible
-            )
+        if (titleResId.isNotEmpty()) {
+            itemsIndexed(titleResId) { index, task ->
+                TaskItem(
+                    task = task,
+                    iconResId = iconId[index],
+                    onTaskClick = {
+                        if (isNextTaskVisible[index]) {
+                            onClick(route[index], letter)
+                        }
+                    },
+                    isClickable = isNextTaskVisible[index],
+                    isCompleted = isTaskCompleted[index]
+                )
+            }
         }
     }
 }
 
 @Composable
-fun TaskItem(
+private fun TaskItem(
     modifier: Modifier = Modifier,
     @StringRes task: Int,
     @DrawableRes iconResId: Int,
     onTaskClick: () -> Unit,
     isClickable: Boolean,
+    isCompleted: Boolean
 ) {
     Card(
         modifier = modifier
@@ -120,6 +128,7 @@ fun TaskItem(
                 Text(
                     fontSize = 32.sp,
                     text = stringResource(task),
+                    color = if (isCompleted) colorProgressBar else colorText,
                     style = MaterialTheme.typography.displayMedium,
                     modifier = modifier
                         .align(Alignment.CenterVertically)
@@ -141,26 +150,18 @@ fun TaskItem(
     }
 }
 
-@Preview(widthDp = 600, heightDp = 400)
+@Preview(widthDp = 600, heightDp = 700)
 @Composable
-fun TaskItemPreview() {
+private fun TaskItemPreview() {
     NivkhAlphabetComposeTheme {
-        /*TaskLayout(
-            task = listOf(
-                TaskModel(
-                    task = Task(
-                        stableId = 1,
-                        titleResId = "",
-                        icon = 0,
-                        route = ""
-                    ),
-                    isTaskCompleted = true,
-
-                    isNextTaskVisible = true
-                )
-            ),
-            letter = "",
+        TaskLayout(
+            titleResId = listOf(R.string.firstTask, R.string.secondTask, R.string.thirdTask),
+            iconId = listOf(R.drawable.ic_task1, R.drawable.ic_task2, R.drawable.ic_task3),
+            route = listOf(),
+            isTaskCompleted = listOf(true, false, false),
+            isNextTaskVisible = listOf(true, true, false),
+            letter = "Aa",
             onClick = { _, _ -> }
-        )*/
+        )
     }
 }
--- app/src/main/java/com/aleksandrgenrikhs/nivkhalphabetcompose/presentation/viewmodels/TasksViewModel.kt
+++ app/src/main/java/com/aleksandrgenrikhs/nivkhalphabetcompose/presentation/viewmodels/TasksViewModel.kt
@@ -2,7 +2,6 @@ package com.aleksandrgenrikhs.nivkhalphabetcompose.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
 import com.aleksandrgenrikhs.nivkhalphabetcompose.Task
-import com.aleksandrgenrikhs.nivkhalphabetcompose.model.TaskModel
 import com.aleksandrgenrikhs.nivkhalphabetcompose.model.interator.AlphabetInteractor
 import com.aleksandrgenrikhs.nivkhalphabetcompose.presentation.uistate.TaskUIState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,41 +13,37 @@ import javax.inject.Inject
 @HiltViewModel
 class TasksViewModel
 @Inject constructor(
-    private val interactor: AlphabetInteractor
+    private val interactor: AlphabetInteractor,
 ) : ViewModel() {
 
-    private val _uiState: MutableStateFlow<TaskUIState> = MutableStateFlow(TaskUIState(
-        task = Task.entries.map {
-            TaskModel(
-                task = it,
-                isTaskCompleted = false,
-                isNextTaskVisible = false
-            )
-        },
-        isNetworkConnected = interactor.isNetWorkConnected()
-    ))
+    private val _uiState: MutableStateFlow<TaskUIState> = MutableStateFlow(
+        TaskUIState(
+            titleResId = Task.entries.map { it.titleResId },
+            iconId = Task.entries.map { it.icon },
+            stableId = Task.entries.map { it.stableId },
+            route = Task.entries.map { it.route },
+            isNetworkConnected = interactor.isNetWorkConnected()
+        )
+    )
     val uiState = _uiState.asStateFlow()
 
     suspend fun isTaskCompleted(letter: String) {
         _uiState.update { uiState ->
-            val taskList = uiState.task.toMutableList()
-            uiState.task.mapIndexed { index, task ->
-                val isTaskCompleted = interactor.isTaskCompleted(
-                    task.task.stableId,
-                    letter
-                )
-                if (isTaskCompleted && index < uiState.task.lastIndex) {
-                    taskList[index + 1] = taskList[index + 1].copy(
-                        isNextTaskVisible = true,
-                    )
+            val newIsTaskCompleted = uiState.isTaskCompleted.toMutableList()
+            val newIsNextTaskVisible = uiState.isNextTaskVisible.toMutableList()
+            uiState.stableId.mapIndexed { index, id ->
+                val isTaskCompleted = interactor.isTaskCompleted(id, letter)
+                newIsTaskCompleted[index] = isTaskCompleted
+
+                if (newIsTaskCompleted[index] && index < uiState.stableId.lastIndex) {
+                    newIsNextTaskVisible[index + 1] = true
                 } else {
-                    taskList[0] = taskList[0].copy(
-                        isNextTaskVisible = true,
-                    )
+                    newIsNextTaskVisible[0] = true
                 }
             }
             uiState.copy(
-                task = taskList,
+                isTaskCompleted = newIsTaskCompleted,
+                isNextTaskVisible = newIsNextTaskVisible
             )
         }
     }

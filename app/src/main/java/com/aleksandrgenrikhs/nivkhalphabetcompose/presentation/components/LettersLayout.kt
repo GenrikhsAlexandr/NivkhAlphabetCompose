2fb96c1d5d766bd4b08e291d8b5189c075b00198
aleksandrgenrikhs <aleksandr.genrikhs@gmail.com>
2024-07-26 23:12:01 +0300

    add image repeat in LettersLayout

--- app/src/main/java/com/aleksandrgenrikhs/nivkhalphabetcompose/presentation/components/LettersLayout.kt
+++ app/src/main/java/com/aleksandrgenrikhs/nivkhalphabetcompose/presentation/components/LettersLayout.kt
@@ -1,9 +1,11 @@
 package com.aleksandrgenrikhs.nivkhalphabetcompose.presentation.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,11 +16,14 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -101,23 +106,31 @@ fun RepeatItem(
     modifier: Modifier = Modifier,
     isVisible: Boolean = true,
 ) {
-    Box(
-        modifier = modifier
-            .height(80.dp)
-            .background(colorPrimary)
-            .clip(CircleShape)
+
+    Column(
+        modifier = Modifier
+            .wrapContentSize()
+            .clip(ShapeDefaults.Medium)
             .clickable(
                 onClick = onClick,
                 enabled = isVisible
             ),
-        contentAlignment = Alignment.Center
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_end_task2),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .height(130.dp)
+        )
         Text(
             text = stringResource(id = R.string.otherTask),
             modifier = modifier
                 .padding(8.dp),
             maxLines = 1,
-            style = MaterialTheme.typography.displayMedium,
+            style = MaterialTheme.typography.titleLarge,
         )
     }
 }
